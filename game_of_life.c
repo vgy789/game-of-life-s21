f04ba5d0e49ecb1d95554da5bd539c61bf38e6a2
Jonna Binns <jonnabin@se-c6.nsk.21-school.ru>
2024-02-04 01:37:48 +0700

    erynda

--- game_of_life.c
+++ game_of_life.c
@@ -1,18 +1,20 @@
 // gcc game_of_life.c -lncurses
-
 #include <ncurses.h>
 #include <stdio.h>
 
 #define N (25)
 #define M (80)
 
-#define DIED ' '
-#define LIVE '#'
+#define DIED ('Z')
+#define LIVE ('#')
 
-#define PRINT_DELAY 1000
+#define PRINT_DELAY (1000)
 
 void initncurses(void);
 void printField(int field[][M]);
+
+void play_life(int field[][M]);
+int count_neighbors(int field[][M], int y, int x);
 
 int main(void) {
     int field[N][M];
@@ -22,22 +24,29 @@ int main(void) {
         }
     }
 
-    field[4][4] = 1;
-    field[5][4] = 1;
-    field[4][5] = 1;
-    
+    field[1][1] = 1;
+    field[2][1] = 1;
+    field[1][2] = 1;
+
+    // считывание данных из файла через циклы и scanf.
+
     initncurses();
     int key_pressed = -1;
 
     do {
         clear();
         printField(field);
+        printw("\n\n>>>%d", count_neighbors(field, 2, 2));
         refresh();
+
+        play_life(field);
+
 
         key_pressed = getch();
         if (key_pressed != -1) {
-
+            // управление, регулирование скорости по нажатию, пауза.
         }
+        
     } while (key_pressed != 'q');
 
     endwin();
@@ -58,6 +67,31 @@ void initncurses(void) {
     // init_pair(3, COLOR_YELLOW, COLOR_YELLOW);
     // init_pair(4, COLOR_GREEN, COLOR_GREEN);
     // init_pair(5, -1, -1);
+}
+
+int count_neighbors(int field[][M], int y, int x) {
+    int row = y-1;
+    int col = x-1;
+    int count = 0;
+
+    for (int i = 0; i< y+1; i++) {
+        for (int j = 0; x+1; j++) {
+           printw("1 ");
+        }
+        printw("\n"); 
+    }
+
+
+    printw("row>>%d", row);
+    return count;
+}
+
+void play_life(int field[][M]) {
+    for (int row = 0; row < N-1; ++row) {
+        for (int col = 0; col < M-1; ++col) {
+
+        }
+    }
 }
 
 void printField(int field[][M]) {
