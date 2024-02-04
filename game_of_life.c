149cd2f296c43ace95c8ddccc083b48affdfa84e
Jonna Binns <jonnabin@pr-d3.nsk.21-school.ru>
2024-02-04 18:47:19 +0700

    a

--- game_of_life.c
+++ game_of_life.c
@@ -7,7 +7,7 @@
 #define N (25)
 #define M (80)
 
-#define PRINT_DELAY 2000
+#define PRINT_DELAY 300
 
 #define DIED (' ')
 #define LIVE ('O')
@@ -15,26 +15,31 @@
 #define LIVE_TAG ('a')
 
 void init_ncurses(void);
-void print_start(); // печатать интро игры
+void print_start();  // печатать интро игры
 int init_field(char field[][M]);
 void play_iteration(char field[][M]);
 int count_neighbors(char field[][M], int y, int x);
 
+void print_intro(void);
+
 void print_field(char field[][M]);
 void copy_field(char dst[][M], const char src[][M]);
-int isgameover(char field[][M]);
+int isgameover(char field[][M], char prev_field[][M]);
 
 int main(void) {
     int key_pressed = -1;
     char field[N][M];
     char prev_field[N][M];
+    int gameover = 0;
 
     int is_loaded = init_field(field);
-    if (!is_loaded) key_pressed == 'q';
+    if (!is_loaded) key_pressed = 'q';
 
     init_ncurses();
+    print_intro();
+    getch();
 
-    while (key_pressed != 'q' && key_pressed != 'Q' && !isgameover(field)) {
+    while (key_pressed != 'q' && key_pressed != 'Q' && !gameover) {
         clear();
         print_field(field);
         refresh();
@@ -42,18 +47,50 @@ int main(void) {
         copy_field(prev_field, field);
         play_iteration(field);
 
+        gameover = isgameover(field, prev_field);
+
         key_pressed = getch();
         if (key_pressed != -1) {
             // управление скоростью
         }
+    }
+    if (gameover) {
+        printw("\t\t\t\tGAME OVER");
+        getch();
     }
 
     endwin();
     return 0;
 }
 
-int isgameover(char field[][M]) {
-    return 0;
+void print_intro(void) {
+    printw("  G)gggg                                O)oooo   f)FFF    L)       ##  f)FFF         \n");
+    printw(" G)                                    O)    oo f)        L)          f)             \n");
+    printw("G)  ggg  a)AAAA   m)MM MMM  e)EEEEE    O)    oo f)FFF     L)       i) f)FFF  e)EEEEE \n");
+    printw("G)    gg  a)AAA  m)  MM  MM e)EEEE     O)    oo f)        L)       i) f)     e)EEEE  \n");
+    printw(" G)   gg a)   A  m)  MM  MM e)         O)    oo f)        L)       i) f)     e)      \n");
+    printw("  G)ggg   a)AAAA m)      MM  e)EEEE     O)oooo  f)        L)llllll i) f)      e)EEEE \n\n");
+
+    printw("\t\t\t\tPress Any Key... ");
+}
+
+int isgameover(char field[][M], char prev_field[][M]) {
+    int is_out = 0;
+    int matches = 0, live_count = 0;
+    char next_field[N][M];
+    copy_field(next_field, field);
+    play_iteration(next_field);
+
+    for (int row = 0; row < N; ++row) {
+        for (int col = 0; col < M; ++col) {
+            live_count += (field[row][col] == LIVE);
+            matches += (prev_field[row][col] == next_field[row][col]);
+        }
+    }
+
+    is_out = (matches == N * M) || (live_count == 0);
+
+    return is_out;
 }
 
 void play_iteration(char field[][M]) {
@@ -98,16 +135,10 @@ int count_neighbors(char field[][M], int y, int x) {
 
 void init_ncurses(void) {
     initscr();
-    // start_color();
-    cbreak();
-    //timeout(PRINT_DELAY);
+    timeout(PRINT_DELAY);
     keypad(stdscr, TRUE);
     noecho();
     curs_set(0);
-    // init_pair(1, COLOR_RED, COLOR_RED);
-    // init_pair(2, COLOR_CYAN, COLOR_CYAN);
-    // init_pair(3, COLOR_YELLOW, COLOR_YELLOW);
-    // init_pair(4, COLOR_GREEN, COLOR_GREEN);
 }
 
 void print_field(char field[][M]) {
@@ -131,14 +162,15 @@ int init_field(char field[][M]) {
         for (int j = 0; j < M; j++) {
             if ((scanf("%d", &number) != 1) || (number != 1 && number != 0)) {
                 printf("The file is damaged");
-                return false;
-            }   else if (number == 1){
+                return 0;
+            } else if (number == 1) {
                 field[i][j] = LIVE;
             }
         }
     }
-    freopen("/dev/tty", "r", stdin);
-    return true;
+    if (freopen("/dev/tty", "r", stdin))
+        ;
+    return 1;
 }
 
 void copy_field(char dst[][M], const char src[][M]) {
